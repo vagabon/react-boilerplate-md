e17d84d92bbd3000a5b0be4054dc51c78b9b867f
GCL <gonzague.clement@gmail.com>
2024-01-14 23:02:04 +0100

    chore: remove unused line on icon index

--- src/icon/index.ts
+++ src/icon/index.ts
@@ -2,5 +2,3 @@ export { default as IconClickable } from './component/IconClickable';
 export type { IIconClickableProps } from './component/IconClickable';
 export { ICONS, useIcon } from './hook/useIcon';
 export type { IconColorType } from './hook/useIcon';
-
-//export * from '@mui/icons-material';
