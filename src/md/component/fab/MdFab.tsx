bd02ebf50634fd33ff1ef4040609ea46824a0030
GCL <gonzague.clement@gmail.com>
2024-04-22 13:04:16 +0200

    feat(fab): update Fab props

--- src/md/component/fab/MdFab.tsx
+++ src/md/component/fab/MdFab.tsx
@@ -1,15 +1,19 @@
-import AddIcon from '@mui/icons-material/Add';
 import { Fab, PropTypes } from '@mui/material';
 import { SyntheticEvent, memo, useCallback } from 'react';
+import { useIcon } from '../../../icon';
 
 export interface IMdFabProps {
+  className?: string;
   size: 'medium' | 'small';
   color: PropTypes.Color | 'success' | 'error' | 'info' | 'warning';
   label?: string;
+  icon?: string;
   callback?: () => void;
 }
 
-const MdFab: React.FC<IMdFabProps> = memo(({ size, color, label, callback }) => {
+const MdFab: React.FC<IMdFabProps> = memo(({ className, size, color, label, icon = 'add', callback }) => {
+  const { getIcon } = useIcon();
+
   const handleClick = useCallback(
     (event: SyntheticEvent<Element, Event>) => {
       event.stopPropagation();
@@ -19,8 +23,8 @@ const MdFab: React.FC<IMdFabProps> = memo(({ size, color, label, callback }) => 
   );
 
   return (
-    <Fab size={size} color={color} aria-label={label} onClick={handleClick}>
-      <AddIcon />
+    <Fab className={className} size={size} color={color} aria-label={label} onClick={handleClick}>
+      {getIcon(icon)}
     </Fab>
   );
 });
