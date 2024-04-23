0aa29e4f6147d2d58100059c2e2429493820cd83
GCL <gonzague.clement@gmail.com>
2024-04-23 18:27:06 +0200

    feat(list): add className props to list component

--- src/md/component/list/MdListItem.tsx
+++ src/md/component/list/MdListItem.tsx
@@ -2,22 +2,26 @@ import { ListItem } from '@mui/material';
 import { ReactNode, memo } from 'react';
 
 export interface IMdListItemProps {
+  className?: string;
   component?: React.ElementType;
   disablePadding?: boolean;
   callback?: () => void;
   isCursor?: boolean;
   children: ReactNode;
 }
-const MdListItem: React.FC<IMdListItemProps> = memo(({ component, disablePadding, callback, isCursor, children }) => {
-  return (
-    <ListItem
-      component={component ?? 'div'}
-      disablePadding={disablePadding ?? true}
-      onClick={callback}
-      style={isCursor ? { cursor: 'pointer' } : {}}>
-      {children}
-    </ListItem>
-  );
-});
+const MdListItem: React.FC<IMdListItemProps> = memo(
+  ({ className, component, disablePadding, callback, isCursor, children }) => {
+    return (
+      <ListItem
+        className={className}
+        component={component ?? 'div'}
+        disablePadding={disablePadding ?? true}
+        onClick={callback}
+        style={isCursor ? { cursor: 'pointer' } : {}}>
+        {children}
+      </ListItem>
+    );
+  },
+);
 
 export default MdListItem;
